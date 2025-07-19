f0f3ff8477e13e00c8b9a9c06fb5e5f2e2406766
Prasanth9080 <prasanthchaandhu02@gmail.com>
2025-07-19 18:31:18 +0530

    Project created file structured and settings.py

--- app/admin.py
+++ app/admin.py
@@ -7,6 +7,23 @@ class UserAdmin(admin.ModelAdmin):
     list_filter = ('is_main_user','is_sub_mainuser') 
     search_fields = ('username', 'phone_number', 'email')
 
+# from django.contrib import admin
+# from django.contrib.auth import get_user_model
+# from .models import MyKYC,BondImage,SubKYC
+
+# User = get_user_model()
+
+# try:
+#     admin.site.unregister(User)
+# except admin.sites.NotRegistered:
+#     pass
+
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ('username', 'email', 'phone_number', 'is_main_user', 'date_joined', 'last_login', 'jwt_token')
+#     list_filter = ('is_main_user', 'is_sub_mainuser')
+#     search_fields = ('username', 'phone_number', 'email')
+
 # ----------------------------------------
 # Admin for MyKYC
 # ----------------------------------------
